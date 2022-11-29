76e03b31f2a3ffe23fe9648c020d21615cc8e027
Edward Tanguay (windows) <edward@tanguay.info>
2022-11-29 11:30:32 +0100

    changed 0 to 1

--- src/apptools.ts
+++ src/apptools.ts
@@ -2,7 +2,7 @@ export const surround = (text: string, twoLetters: string) => {
 	if (twoLetters.length !== 2) {
 		return text;
 	} else {
-		const first = twoLetters[0];
+		const first = twoLetters[1];
 		const last = twoLetters[1];
 		return first + text + last;
 	}
